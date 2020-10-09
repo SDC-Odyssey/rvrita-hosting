942000777820ee6add8cc925174d6464985db20b
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-10-09 12:49:32 -0700

    Refactor seeding using 1M batches and spawnSync

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,3 +1,37 @@
+const generateHostProfiles = require('../helpers/generateHostProfiles')
+const { spawnSync } = require('child_process');
+
+const numberOfRecordsToInsert = 10000000;
+const batchSize = 1000000;
+let recordsRemaining = numberOfRecordsToInsert;
+let startingId = 0;
+
+console.log('Seeding %d records with batch size %d.', numberOfRecordsToInsert, batchSize);
+
+while (recordsRemaining > 0) {
+  const currentBatchSize = Math.min(recordsRemaining, batchSize);
+
+  console.log(`Generating ${currentBatchSize} records...`);
+  const sampleHostProfiles = generateHostProfiles(currentBatchSize, startingId);
+  console.log('Importing records to mongo...');
+  const output = spawnSync(
+    'mongoimport',
+    [ startingId === 0 ? '--drop' : '', '--jsonArray', '--collection=hostprofiles',
+      '--uri="mongodb://localhost/hostInfo"', '--numInsertionWorkers=8'],
+    { input: JSON.stringify(sampleHostProfiles) }
+  );
+  console.log(output.stderr.toString());
+
+  recordsRemaining -= currentBatchSize;
+  startingId += currentBatchSize;
+}
+
+
+
+
+
+/*
+
 // const mongoose = require('mongoose');
 // const HostProfile = require('./models/hostProfile');
 const generateHostProfiles = require('../helpers/generateHostProfiles')
@@ -39,3 +73,5 @@ while (recordsRemaining > 0) {
 
 // null to end stream (like EOF - end of file)
 s.push(null);
+
+*/
