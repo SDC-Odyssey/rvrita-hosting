5be2dab96455131f8d9b34f95bfea0871bea05f6
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-10-15 23:25:38 -0700

    WIP seeding with postgres

---
+++ server/db/models/schema.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE hostinfo (
+  id SERIAL NOT NULL PRIMARY KEY,
+  host_url TEXT,
+  host_name TEXT,
+  cohost_name TEXT,
+  host_about TEXT,
+  host_messages TEXT,
+  host_identity_verified BOOLEAN,
+  host_is_superHost BOOLEAN,
+  host_has_profile_pic BOOLEAN,
+  host_has_coHost BOOLEAN,
+  host_response_time INTEGER,
+  host_listings_count INTEGER,
+  host_verifications TEXT[],
+  host_languages TEXT[],
+  modified_at TIMESTAMP,
+  created_at TIMESTAMP
+);
--- server/db/seed.js
+++ server/db/seed.js
@@ -1,73 +1,98 @@
 // more info in the journal
-const generateHostProfiles = require('../helpers/generateHostProfiles')
-const { spawnSync } = require('child_process');
+// const generateHostProfiles = require('../helpers/generateHostProfiles')
+// const { spawnSync } = require('child_process');
 
-const numberOfRecordsToInsert = 4000000;
-const batchSize = 400000;
-let recordsRemaining = numberOfRecordsToInsert;
-let startingId = 0;
+// const numberOfRecordsToInsert = 10000000;
+// const batchSize = 1000000;
+// let recordsRemaining = numberOfRecordsToInsert;
+// let startingId = 0;
 
-console.log('Seeding %d records with batch size %d.', numberOfRecordsToInsert, batchSize);
+// console.log('Seeding %d records with batch size %d.', numberOfRecordsToInsert, batchSize);
 
-while (recordsRemaining > 0) {
-  const currentBatchSize = Math.min(recordsRemaining, batchSize);
+// while (recordsRemaining > 0) {
+//   const currentBatchSize = Math.min(recordsRemaining, batchSize);
 
-  console.log(`Generating ${currentBatchSize} records...`);
-  const sampleHostProfiles = generateHostProfiles(currentBatchSize, startingId);
-  console.log('Importing records to mongo...');
-  const output = spawnSync(
-    'mongoimport',
-    [ startingId === 0 ? '--drop' : '', '--jsonArray', '--collection=hostprofiles',
-      '--uri="mongodb://localhost/hostInfo"', '--numInsertionWorkers=8'],
-    { input: JSON.stringify(sampleHostProfiles) }
-  );
-  console.log(output.stderr.toString());
+//   console.log(`Generating ${currentBatchSize} records...`);
+//   const sampleHostProfiles = generateHostProfiles(currentBatchSize, startingId);
+//   console.log('Importing records to mongo...');
+//   const output = spawnSync(
+//     'mongoimport',
+//     [ startingId === 0 ? '--drop' : '', '--jsonArray', '--collection=hostprofiles',
+//       '--uri="mongodb://localhost/hostInfo"', '--numInsertionWorkers=8'],
+//     { input: JSON.stringify(sampleHostProfiles) }
+//   );
+//   console.log(output.stderr.toString());
 
-  recordsRemaining -= currentBatchSize;
-  startingId += currentBatchSize;
-}
+//   recordsRemaining -= currentBatchSize;
+//   startingId += currentBatchSize;
+// }
 
-/*
 
-// const mongoose = require('mongoose');
-// const HostProfile = require('./models/hostProfile');
-const generateHostProfiles = require('../helpers/generateHostProfiles')
-const { spawn } = require('child_process');
+const generateHostProfiles = require('../helpers/generateHostProfiles');
 const fs = require('fs');
-const Readable = require('stream').Readable;
+const path = require('path');
+require('dotenv').config();
+const schema = fs.readFileSync(path.resolve(__dirname, './models/schema.sql')).toString();
+const queries = schema.split(';\n');
 
-// turns a plain string into a stream
+// ------------------ pg-promise
 
-
-const numberOfRecordsToInsert = 1000000;
-const batchSize = 100000;
-let recordsRemaining = numberOfRecordsToInsert;
-let startingId = 0;
-
-console.log('Seeding %d records with batch size %d.', numberOfRecordsToInsert, batchSize);
-const proc = spawn('mongoimport', ['--drop', '--jsonArray', '--collection=hostprofiles', '--uri="mongodb://localhost/hostInfo"', '--numInsertionWorkers=8']);
-// To see mongoimport activity in the console
-proc.stderr.on('data', (data) => {
-  console.warn(data.toString());
+const pgp = require('pg-promise')();
+const cn = {
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  max: 30 // use up to 30 connections
+};
+const db = pgp(cn);
+// console.log('query:',schema);
+db.task(async t => {
+  for (let i = 0; i < queries.length; i++) {
+    console.log('!!!',queries[i]);
+    await t.one(queries[i]);
+  }
+})
+.then(events => {
+  console.log('Events:', events);
+})
+.catch(error => {
+  console.log('ERROR:', error);
 });
-const s = new Readable();
 
-while (recordsRemaining > 0) {
-  const currentBatchSize = Math.min(recordsRemaining, batchSize);
+// db.multi(schema)
+//     .then(data => {
+//         console.log(data.id); // print new user id;
+//     })
+//     .catch(error => {
+//         console.log('ERROR:', error); // print error;
+//     });
 
-  console.log(`Generating ${currentBatchSize} records...`);
-  const sampleHostProfiles = generateHostProfiles(currentBatchSize, startingId);
-  console.log('Importing records to mongo...');
-  // proc.stdin.write(JSON.stringify(sampleHostProfiles));
-  // fs.writeFileSync('hostProfiles.json', JSON.stringify(sampleHostProfiles));
-  s.push(JSON.stringify(sampleHostProfiles));
-  // pipe the JSON string stream to mongoimport
-  s.pipe(proc.stdin);
+db.one('INSERT INTO hostinfo(host_name, cohost_name) VALUES($1, $2)', ['John', 'Hi'])
+    .then(data => {
+        console.log(data); // print new user id;
+    })
+    .catch(error => {
+        console.log('ERROR:', error); // print error;
+    });
 
-  recordsRemaining -= currentBatchSize;
-  startingId += currentBatchSize;
-}
 
-// null to end stream (like EOF - end of file)
-s.push(null);
-*/
+// ---------------- node-postgres
+
+// const { Pool } = require('pg');
+// const pool = new Pool();
+// pool
+//   .connect()
+//   .then(client => {
+//     return client
+//       .query('DROP TABLE IF EXISTS hostinfo')
+//       .then(res => {
+//         client.release();
+//         console.log(res.rows[0]);
+//       })
+//       .catch(err => {
+//         client.release();
+//         console.log(err.stack);
+//       });
+//   });
